c7c9d27a870e1ec0727072fa7d89aa7083902009
Belal Hmeidat <belalhmeidat@icloud.com>
2023-08-06 16:46:20 +0300

    Initial Commit

---
+++ DeviceChooser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeviceChooser
+//
+//  Created by Belal Hmeidat on 8/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
