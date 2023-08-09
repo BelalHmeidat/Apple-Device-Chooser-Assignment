98d7c42b630f1f3ad3785c67ee5738eb40a414f7
Belal Hmeidat <belalhmeidat@icloud.com>
2023-08-09 13:18:56 +0300

    Update SheetViewController.swift

--- DeviceChooser/SheetViewController.swift
+++ DeviceChooser/SheetViewController.swift
@@ -29,15 +29,15 @@ class SheetViewController: UIViewController {
         view.backgroundColor = .white
 
         view.addSubview(iphoneButton)
-        layoutButton(button: iphoneButton)
+        layoutButton(button: iphoneButton, space: 100)
         addFuncToButton(button: iphoneButton, action: #selector(iphoneButtonAction))
     
         view.addSubview(ipadButton)
-        layoutButton(button: ipadButton)
+        layoutButton(button: ipadButton, space: 180)
         addFuncToButton(button: ipadButton, action: #selector(ipadButtonAction))
         
         view.addSubview(macButton)
-        layoutButton(button: macButton)
+        layoutButton(button: macButton, space: 260)
         addFuncToButton(button: macButton, action: #selector(macButtonAction))
 
         
@@ -56,29 +56,18 @@ class SheetViewController: UIViewController {
         return button
     }
     
-    func layoutButton(button : UIButton){
+    func layoutButton(button : UIButton, space: Int){
         
-        if buttonCount == 0{
             
-            NSLayoutConstraint.activate([
-                button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-                button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                button.widthAnchor.constraint(equalToConstant: CGFloat(btWidth)),
-                button.heightAnchor.constraint(equalToConstant: CGFloat(btHeight))
-            ])
-        }
-        else {
-            let topAnchorElement : AnyObject = TintedButton.buttons[buttonCount-1]
-            NSLayoutConstraint.activate([
-                button.topAnchor.constraint(equalTo: (topAnchorElement.bottomAnchor) as NSLayoutAnchor<NSLayoutYAxisAnchor>, constant: CGFloat(distanceBetweenButtons)),
-                button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                button.widthAnchor.constraint(equalToConstant: CGFloat(btWidth)),
-                button.heightAnchor.constraint(equalToConstant: CGFloat(btHeight))
-            ])
-            
-        }
-        buttonCount += 1
+        NSLayoutConstraint.activate([
+            button.centerYAnchor.constraint(equalTo: view.topAnchor, constant: CGFloat(space)),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: CGFloat(btWidth)),
+            button.heightAnchor.constraint(equalToConstant: CGFloat(btHeight))
+        ])
     }
+        
+
     
     func addFuncToButton(button: UIButton, action: Selector){
         button.addTarget(self, action: action, for: .touchUpInside)
@@ -90,19 +79,24 @@ class SheetViewController: UIViewController {
         guard let image = UIImage(named: "iphonex_front_back_glass_big.jpg.large.jpg") else {return}
         delegate?.didSelectOption(image: image, labelText: "iPhone")
         dismiss(animated: true, completion: nil)
+        buttonCount = 0
     }
     
     @objc func ipadButtonAction(_ sender: UIButton){
         guard let image = UIImage(named: "g4SGRalYN2leGXDf.medium.jpg") else {return}
         delegate?.didSelectOption(image: image, labelText: "iPad")
         dismiss(animated: true, completion: nil)
+        buttonCount = 0
+
     }
     
     @objc func macButtonAction(_ sender: UIButton){
         guard let image = UIImage(named: "macbook-2048px-9765.jpg") else {return}
         delegate?.didSelectOption(image: image, labelText: "Macbook")
         dismiss(animated: true, completion: nil)
+        buttonCount = 0
+
     }
+}
 
     
-}
