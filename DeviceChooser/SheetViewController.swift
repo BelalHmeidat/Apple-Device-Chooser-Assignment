cd447b0b5d3d5781e9424df2a3ec47fc912dc341
Belal Hmeidat <belalhmeidat@icloud.com>
2023-08-17 12:19:18 +0300

    Used class instead of enum and added index to perform button actions

--- DeviceChooser/SheetViewController.swift
+++ DeviceChooser/SheetViewController.swift
@@ -12,106 +12,124 @@ protocol SheetViewControllerDelegate: AnyObject {
     func didSelectOption(image: UIImage, labelText: String)
 }
 
+class Product {
+    var name : String
+    var image : UIImage
+    var icon : UIImage
+    var color : UIColor
+    
+    init(name: String, image: UIImage, icon: UIImage, color: UIColor) {
+        self.name = name
+        self.image = image
+        self.icon = icon
+        self.color = color
+    }
+    
+    
+}
+
 
 class SheetViewController: UIViewController {
-    enum Product {
-        case iPhone
-        case iPad
-      case macbook
-        
-        var name: String {
-            switch self {
-            case .iPhone:
-                return "iPhone"
-            case .iPad:
-                return "iPad"
-            case .macbook:
-                return "Macbook"
-            }
-        }
-        var image : UIImage{
-            switch self{
-            case .iPhone :
-                return UIImage(named: "iPhone.jpg")!
-            case .iPad :
-                return UIImage(named: "iPad.jpeg")!
-            case .macbook :
-                return UIImage(named: "Macbook.jpg")!
-            }
-        }
-        var icon : UIImage {
-            switch self {
-            case .iPhone :
-                return UIImage(systemName: "iphone")!
-            case .iPad :
-                return UIImage(systemName: "ipad.gen1")!
-            case .macbook:
-                return UIImage(systemName: "laptopcomputer")!
-            }
-        
-        }
-        var color : UIColor {
-            switch self{
-            case .iPhone :
-                 return UIColor(named: "iPhoneColor")!
-                
-            case .iPad :
-                return UIColor(named: "iPadColor")!
-            case .macbook :
-                return UIColor(named: "MacColor")!
-            }
-        }
-        
-        var action : Selector{
-            switch self {
-            case .iPhone :
-                return #selector(iphoneButtonAction)
-                
-            case .iPad :
-                return #selector(ipadButtonAction)
-            case .macbook :
-                return #selector(macButtonAction)
-            }
-        }
-      
-    }
-    var products : [Product] = [.iPhone, .iPad, .macbook]
+    
+    
+    
+    
+    var iPhone = Product(name: "iPhone", image: UIImage(named: "iPhone.jpg")!, icon: UIImage(systemName: "iphone")!, color: UIColor(named: "iPhoneColor")!)
+    var iPad = Product(name: "iPad", image: UIImage(named: "iPad.jpeg")!, icon: UIImage(systemName: "ipad.gen1")!, color: UIColor(named: "iPadColor")!)
+    var macbook = Product(name: "Macbook", image: UIImage(named: "Macbook.jpg")!, icon: UIImage(systemName: "laptopcomputer")!, color: UIColor(named: "MacColor")!)
+    
+//    enum Product {
+//        case iPhone
+//        case iPad
+//        case macbook
+//
+//        var name: String {
+//            switch self {
+//            case .iPhone:
+//                return "iPhone"
+//            case .iPad:
+//                return "iPad"
+//            case .macbook:
+//                return "Macbook"
+//            }
+//        }
+//        var image : UIImage{
+//            switch self{
+//            case .iPhone :
+//                return UIImage(named: "iPhone.jpg")!
+//            case .iPad :
+//                return UIImage(named: "iPad.jpeg")!
+//            case .macbook :
+//                return UIImage(named: "Macbook.jpg")!
+//            }
+//        }
+//        var icon : UIImage {
+//            switch self {
+//            case .iPhone :
+//                return UIImage(systemName: "iphone")!
+//            case .iPad :
+//                return UIImage(systemName: "ipad.gen1")!
+//            case .macbook:
+//                return UIImage(systemName: "laptopcomputer")!
+//            }
+//
+//        }
+//        var color : UIColor {
+//            switch self{
+//            case .iPhone :
+//                return UIColor(named: "iPhoneColor")!
+//
+//            case .iPad :
+//                return UIColor(named: "iPadColor")!
+//            case .macbook :
+//                return UIColor(named: "MacColor")!
+//            }
+//        }
+//
+//        var action : Selector{
+//            switch self {
+//            case .iPhone :
+//                return #selector(iphoneButtonAction)
+//
+//            case .iPad :
+//                return #selector(ipadButtonAction)
+//            case .macbook :
+//                return #selector(macButtonAction)
+//            }
+//        }
+//
+//    var products : [Product] = [.iPhone, .iPad, .macbook]
+    var products : [Product] = []
     
     
     weak var delegate: SheetViewControllerDelegate?
     
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let iPhone = Product(name: "iPhone", image: UIImage(named: "iPhone.jpg")!, icon: UIImage(systemName: "iphone")!, color: UIColor(named: "iPhoneColor")!)
+        let iPad = Product(name: "iPad", image: UIImage(named: "iPad.jpeg")!, icon: UIImage(systemName: "ipad.gen1")!, color: UIColor(named: "iPadColor")!)
+        let macbook = Product(name: "Macbook", image: UIImage(named: "Macbook.jpg")!, icon: UIImage(systemName: "laptopcomputer")!, color: UIColor(named: "MacColor")!)
+        products = [iPhone, iPad, macbook]
         view.backgroundColor = .white
         createAllButtons()
-
+        
     }
     
     func createAllButtons(){
         var i = 1
         for product in products{
-            let bt = makeButton(product: product)
+            let bt = makeButton(product: product, index: i)
             view.addSubview(bt)
             layoutButton(button: bt, space: CGFloat(100 * i))
-            bt.addTarget(self, action: product.action, for: .touchUpInside)
+            bt.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
             i+=1
         }
     }
-
-    func makeButton(product: Product) -> UIButton{
-//        let button = UIButton()
-//        button.configuration = .tinted()
-//        button.configuration?.title = product.name
-//        button.configuration?.baseBackgroundColor = product.color
-//        button.configuration?.baseForegroundColor = product.color
-//        button.configuration?.image = product.icon
-//        button.configuration?.cornerStyle = .small
-//        button.translatesAutoresizingMaskIntoConstraints = false
-        let button = TintedButton(title: product.name, icon: product.icon, bgColor: product.color)
-
+    
+    func makeButton(product: Product, index: Int) -> UIButton{
+        let button = TintedButton(title: product.name, icon: product.icon, bgColor: product.color, index: index)
+        
         
         return button
     }
@@ -129,33 +147,24 @@ class SheetViewController: UIViewController {
     }
     
     
-
-    @objc func iphoneButtonAction(_ sender: UIButton){
-        delegate?.didSelectOption(image: Product.iPhone.image, labelText: Product.iPhone.name)
+    @objc func buttonClicked(_ sender: TintedButton){
+        switch sender.index {
+        case 1:
+            delegate?.didSelectOption(image:iPhone.image, labelText: iPhone.name)
+        case 2:
+            delegate?.didSelectOption(image: iPad.image, labelText: iPad.name)
+        case 3:
+            delegate?.didSelectOption(image: macbook.image, labelText: macbook.name)
+        default:
+            break
+        }
+        
         dismiss(animated: true, completion: nil)
-    
-    }
-    
-    @objc func ipadButtonAction(_ sender: UIButton){
-        delegate?.didSelectOption(image: Product.iPad.image, labelText: Product.iPad.name)
-        dismiss(animated: true, completion: nil)
-    
-
-    }
-    
-    @objc func macButtonAction(_ sender: UIButton){
-        delegate?.didSelectOption(image: Product.macbook.image, labelText: Product.macbook.name)
-        dismiss(animated: true, completion: nil)
-    
-
     }
     
     
-// Doesn't work
-//     @objc func buttonFunction(image : UIImage, text: String, _ sender : UIButton){
-//        delegate?.didSelectOption(image: image, labelText: text)
-//        dismiss(animated: true, completion: nil)
-//    }
+    
+    
 }
 
-    
+
--- DeviceChooser/TintedButton.swift
+++ DeviceChooser/TintedButton.swift
@@ -20,9 +20,13 @@ class TintedButton: UIButton{
         fatalError("init(coder:) has not been implemented")
     }
     
-//    static var buttons : [UIButton] = []
+//    static var products : [Product] = []
+    static var indexes : [Int] = []
     
-    init(title: String, icon: UIImage, bgColor: UIColor ){
+    let index : Int
+    
+    init(title: String, icon: UIImage, bgColor: UIColor, index : Int ){
+        self.index = index
         super.init(frame: .zero)
         configuration = .tinted()
         configuration?.title = title
@@ -31,7 +35,13 @@ class TintedButton: UIButton{
         configuration?.baseForegroundColor = bgColor
         configuration?.cornerStyle = .small
         translatesAutoresizingMaskIntoConstraints = false
+//        self.tintColor = bgColor
+//        self.setTitle(title, for: .normal)
+        
 //        TintedButton.buttons.append(self)
+        
     }
+    
+
     
 }
