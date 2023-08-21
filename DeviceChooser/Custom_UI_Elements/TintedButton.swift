597f69577507093afad81c1ed20cc97a3ac8a317
Belal Hmeidat <belalhmeidat@icloud.com>
2023-08-21 15:48:41 +0300

    Added constraints, used struct instead of class, added documentation, added access control, added getters instead of property direct access, cleaned up code, removed redundant code, moved code out of the viewdidload function, made buttons background tinted-sh color.

---
+++ DeviceChooser/Custom_UI_Elements/TintedButton.swift
@@ -0,0 +1,36 @@
+//
+//  SheetViewController.swift
+//  DeviceChooser
+//
+//  Created by Belal Hmeidat on 8/7/23.
+//
+
+
+
+import UIKit
+//custom class to make UIButton tinted and is created with an index to be used in setting button actions
+class TintedButton: UIButton{
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    let index : Int //to identify buttons for actions
+    
+    init(title: String, icon: UIImage, bgColor: UIColor, index : Int ){
+        self.index = index
+        super.init(frame: .zero)
+        self.frame = CGRect(x: 100, y: 200, width: 200, height: 50) //icon gets broken without this
+        self.setTitle(title, for: .normal)
+        self.layer.cornerRadius = 8
+        self.setTitleColor(bgColor, for: .normal)
+        self.backgroundColor = bgColor.withAlphaComponent(0.1) //same as title color but lighter (adjusted using alpha)
+        self.setImage(icon, for: .normal)
+        self.tintColor = bgColor //needed to make the icon tinted otherwise it will be default color
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0,  right: 15) //space between the icon and the text of the button
+        translatesAutoresizingMaskIntoConstraints = false //constraints will be manually set
+    }
+    
+
+    
+}
